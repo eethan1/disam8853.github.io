a735bcae765320e519358039f16e9deca4f663fb
Sam <wang.sam33@gmail.com>
2019-01-31 17:36:09 +0800

    add information css

--- imcamp/information/script.js
+++ imcamp/information/script.js
@@ -1,5 +1,22 @@
 $(window).on('load', function() {
-    $(".white-light").fadeOut(1000, function(){
-    	$(".landed-rocket-img").css("transform", "translateY(12vmin) rotate(-73deg)");
+    $(".white-light").fadeOut(1000, function() {
+        $(".landed-rocket-img").css("transform", "translateY(12vmin) rotate(-73deg)");
     });
+});
+
+$(function() {
+    $('#content').pagination({
+        dataSource: [1, 2, 3, 4, 5, 6, 7],
+        pageSize: 1,
+        showPageNumbers: false,
+        showNavigator: true,
+        callback: function(data, pagination) {
+            // template method of yourself
+            // dataContainer.html(html);
+            $(".page-container").fadeOut(500, function() {
+                $(this).append(data)
+                    .fadeIn(500);
+            });
+        }
+    })
 });
